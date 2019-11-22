c8b8b58824f4b7df54ef5d708d4af103e31d8b04
Dillan Teagle <dteagle@cpicardgroup.com>
2019-11-22 10:14:24 -0600

    recieve metaclass

--- lib/recieve.py
+++ lib/recieve.py
@@ -1,19 +1,54 @@
 import pika
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+import ast
 
 
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+class Singleton(type):
+
+    _instance ={}
+
+    def __call__(cls, *args, **kwargs):
+
+        """ Singelton Design Pattern  """
+
+        if cls not in cls._instance:
+            cls._instance[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+class RabbitMqServerConfigure(metaclass=Singleton):
 
-    
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
+    def __init__(self, host, queue):
+        self.host = host
+        self.queue = queue
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+
+class Server():
+
+    def __init__(self, server):
+        self.server = server
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(queue=self.server.queue)
+
+    @staticmethod
+    def callback(ch, method, properties, body):
+        payload = body.decode("utf-8")
+        payload = ast.literal_eval(payload)
+        print(type(payload))
+        print("Data Recieved: {}".format(payload))
+
+    def start_server(self):
+        self._channel.basic_consume(
+            queue=self.server.queue,
+            on_message_callback=Server.callback,
+            auto_ack=True
+        )
+        print("Listening..")
+        self._channel.start_consuming()
+
+
+
+if __name__ == "__main__":
+    settings = RabbitMqServerConfigure(host='localhost', queue='hello')
+    server = Server(server=settings)
+    server.start_server()
